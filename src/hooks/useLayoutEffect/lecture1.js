0284f185ae28a3ffa7793883c586d5c375034748
Kenichi Okiebisu <a01056715@gmail.com>
2020-10-13 15:51:16 -0700

    lecture1修正

--- src/hooks/useLayoutEffect/lecture1.js
+++ src/hooks/useLayoutEffect/lecture1.js
@@ -1,4 +1,4 @@
-import React, { useRef, useLayoutEffect } from 'react';
+import React, { useRef, useState, useLayoutEffect } from 'react';
 
 /**
  * useLayoutEffectはuseEffectと同じでDOMがマウントされる時に呼ばれますが、useLayoutEffectはDOMがマウントされた瞬間ほぼ同時に呼ばれます。
@@ -6,6 +6,7 @@ import React, { useRef, useLayoutEffect } from 'react';
  * を測る時に使われます。その例を下記で紹介します。
  */
 export const Lecture1 = () => {
+  const [info, setInfo] = useState({});
   const containerRef = useRef();
   /**
    * getBoundingClientRectはdomに備わっているメソッドで要素の大きさを測ってくれます。
@@ -13,10 +14,21 @@ export const Lecture1 = () => {
    */
   useLayoutEffect(() => {
     console.log(containerRef.current.getBoundingClientRect());
-  });
+    setInfo(containerRef.current.getBoundingClientRect());
+  }, []);
   return (
-    <div ref={containerRef}>
-      ここではこの文章を囲っているdivの長さを計りコンソールに表示させています。
-    </div>
+    <>
+      <div style={{ display: 'inline-block' }} ref={containerRef}>
+        ここではこの文章を囲っているdivの長さを計りコンソールに表示させていますよね。
+      </div>
+      <div>文章のwidthは：{JSON.stringify(info.width)}pxです</div>
+      <div>文章のheightは：{JSON.stringify(info.height)}pxです</div>
+      <div>文章のx座標は：{JSON.stringify(info.x)}です</div>
+      <div>文章のy座標は：{JSON.stringify(info.y)}です</div>
+      <div>文章は右から{JSON.stringify(info.right)}pxのところにあります</div>
+      <div>文章は左から：{JSON.stringify(info.left)}pxのところにあります</div>
+      <div>文章は上から{JSON.stringify(info.top)}pxのところにあります</div>
+      <div>文章は下から：{JSON.stringify(info.bottom)}pxのところにあります</div>
+    </>
   );
 };
